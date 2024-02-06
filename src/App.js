4ead9093660d571a29d1926c344a7437bc48147c
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-06 14:22:55 +0000

    specific key for each movie

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard movie={movie} key={movie.Poster} />
           ))}
         </div>
       ) : (
